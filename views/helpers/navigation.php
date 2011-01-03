0c45a2d401c7898d642db8282392c03b438a57e9
Sam S <sam.sherlock@gmail.com>
2011-01-03 19:53:51 +0000

    alter config.ini setup and helper

--- views/helpers/navigation.php
+++ views/helpers/navigation.php
@@ -5,59 +5,57 @@
   */
  
  class NavigationHelper extends AppHelper {
-	public $helpers = array('Html');
+  public $helpers = array('Html');
 
 /**
  * Disable autoInclusion of view js files.
  *
  * @var string
  */
-	public $autoInclude = true;
+  public $autoInclude = true;
 
 /**
  * parsed ini file values.
  *
  * @var array
  */
-	protected $_currentView;
+  protected $_currentView;
 
 /**
  * parsed ini file values.
  *
  * @var array
  */
-	protected $_iniFile;
+  protected $_iniFile;
 
 /**
  * Contains the build timestamp from the file.
  *
  * @var string
  */
-	protected $_buildTimestamp;
-    
+  protected $_buildTimestamp;  
+
 /**
  * Constructor - finds and parses the ini file the plugin uses.
  *
  * @return void
  */
-	public function __construct($options = array()) {
-		if (!empty($options['iniFile'])) {
-			$iniFile = $options['iniFile'];
-		} else {
-			$iniFile = CONFIGS . 'navigation.ini';
-		}
-		if (!file_exists($iniFile)) {
-			$iniFile = App::pluginPath('Navigation') . 'config' . DS . 'config.ini';
-		}
-		$this->_iniFile = parse_ini_file($iniFile, true);
-                
-                $this->_currentView = &ClassRegistry::getObject('view');
-	
-        //die(debug($this->_iniFile));
-        //die(debug($this));
-        }
-        
-    
+ public function __construct($options = array()) {
+  if (!empty($options['iniFile'])) {
+	  $iniFile = $options['iniFile'];
+  } else {
+	  $iniFile = CONFIGS . 'navigation.ini';
+  }
+  if (!file_exists($iniFile)) {
+	  $iniFile = App::pluginPath('Navigation') . 'config' . DS . 'config.ini';
+  }
+  $this->_iniFile = parse_ini_file($iniFile, true);
+  
+  $this->_currentView = &ClassRegistry::getObject('view');
+ 
+ //die(debug($this->_iniFile));
+ //die(debug($this));
+ }
 
 /**
  * Modify the runtime configuration of the helper.
@@ -67,70 +65,61 @@
  * @param mixed $value The value to set the config to.
  * @return mixed Either the value being read or null.  Null also is returned when reading things that don't exist.
  */
-	public function config($name, $value = null) {
-		if (strpos($name, '.') === false) {
-			return null;
-		}
-		list($section, $key) = explode('.', $name);
-		if ($value === null) {
-			return isset($this->_iniFile[$section][$key]) ? $this->_iniFile[$section][$key] : null;
-		}
-		$this->_iniFile[$section][$key] = $value;
-	}
+ public function config($name, $value = null) {
+  if (strpos($name, '.') === false) {
+	  return null;
+  }
+  list($section, $key) = explode('.', $name);
+  if ($value === null) {
+	  return isset($this->_iniFile[$section][$key]) ? $this->_iniFile[$section][$key] : null;
+  }
+  $this->_iniFile[$section][$key] = $value;
+ }
 
 /**
  * Set options, merge with existing options.
  *
  * @return void
  */
-	public function options($options) {
-		$this->options = Set::merge($this->options, $options);
-	}
+ public function options($options) {
+   $this->options = Set::merge($this->options, $options);
+ }
+ 
+ /*
+  * function _get_setup
+  * @param 
+  */
+ 
+ function _get_setup() {
+  $i = 0;
+  $navigations = false;
+  $naviArr = array();
+  $naviArr = $this->_iniFile['Navigation.Menus'];
+  
+   return array_keys($naviArr);
+ }
  
   /*
    * function _get_navigation
    * @param 
    */
   
-  function _get_navigation() {
+  function _get_navigation($name) {
    $i = 0;
-   $main = $sidebar = $foot = false;
-   $mainArr = $sidebarArr = $footArr = array();
+   $Arr = $keys = $menu = array();
    //print_r($this->_iniFile);die();
-   if(array_key_exists('Main', $this->_iniFile)) {
-    $mainArr = $this->_iniFile['Main'];
+   if(array_key_exists(ucfirst($name), $this->_iniFile)) {
+    $Arr = $this->_iniFile[ucfirst($name)];
     
-    $mainKeys = array_keys($mainArr);
+    $Keys = array_keys($Arr);
     
-    foreach($mainKeys as $keyIndex => $keyValue) {
-     $main[$keyIndex]['display'] = $keyValue;
-     $main[$keyIndex]['link'] = Set::extract($keyValue, $mainArr);
+    foreach($Keys as $keyIndex => $keyValue) {
+     $menu[$keyIndex]['display'] = $keyValue;
+     $menu[$keyIndex]['link'] = Set::extract($keyValue, $Arr);
     }
    }
    
-   if(array_key_exists('Foot', $this->_iniFile)) {
-    $footArr = $this->_iniFile['Foot'];
-    
-    $footKeys = array_keys($footArr);
-    
-    foreach($mainKeys as $keyIndex => $keyValue) {
-     $foot[$keyIndex]['display'] = $keyValue;
-     $foot[$keyIndex]['link'] = Set::extract($keyValue, $footArr);
-    }
-   }
-   
-   if(array_key_exists('Sidebar', $this->_iniFile)) {
-    $sidebarArr = $this->_iniFile['Foot'];
-    
-    $sidebarKeys = array_keys($footArr);
-    
-    foreach($mainKeys as $keyIndex => $keyValue) {
-     $sidebar[$keyIndex]['display'] = $keyValue;
-     $sidebar[$keyIndex]['link'] = Set::extract($keyValue, $sidebarArr);
-    }
-   }
-   
-   return compact('main', 'foot', 'sidebar');
+   return compact('menu');
   }  
     
     /*
@@ -139,7 +128,11 @@
      */
     
     function beforeRender() {
-        $this->_currentView->viewVars['navigation_for_layout'] = $this->_get_navigation();
+       $menus = array();
+       foreach($this->_get_setup() as $menu) {
+	$menus[] = $this->_get_navigation($menu);
+       }
+        $this->_currentView->viewVars['navigation_for_layout'] = $menus;
     }
     
     /*
@@ -153,22 +146,21 @@
      
      
      $_items = ''; $_options = array(); $link = $display = '';
-     //print_r($this->_currentView->params);die();
-     //print_r($this->_currentView->viewVars);//die();
-     //$items = Set::classicExtract($this->_currentView->viewVars, 'navigation_for_layout.*');
-     //Set::extract()
-     //print_r($array_items);
-     $items = Set::extract('/navigation_for_layout/main', $this->_currentView->viewVars);
-     $hitems = $array_items;//Set::extract('main', $array_items);
-     //print_r($items);//die();
-     //print_r($hitems);//die();
-     $items = array_merge($items, $hitems);
-     //print_r($items);die();
      
-     foreach($items[0]['main'] as $key => $item) {
-      //print_r($key);
-      //print_r($item);
-      $_items.= $this->Html->link($item['display'], $item['link'], $_options);
+     $items = $this->_iniFile[ucfirst($section).'.links'];
+     $titles = $this->_iniFile[ucfirst($section).'.titles'];
+     foreach($array_items as $k => $v) {
+      $hitems[$v['display']] = $v['link'];
+      if(isset($v['title'])) {
+       $titles[$v['display']] = $v['title'];
+      }
+     }
+     $items = array_merge($items, $hitems);
+     
+     
+     foreach($items as $key => $item) {
+      $options = (isset($titles[$key])) ? array_merge($_options, array('title' => $titles[$key])) : null;
+      $_items.= $this->Html->link($key, $item, $options);
      }
      return $_items;
     }
